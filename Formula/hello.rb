edde950686725e0dba6dbfe5af40ab689003ae8e
Dan Bamikiya <dudeawesome732@gmail.com>
2023-08-26 19:16:02 +0100

    fix(formula): make the sha256 command on the same line as the value

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -2,8 +2,7 @@ class Hello < Formula
     desc "A simple greeter"
     homepage "https://github.com/danBamikiya/hello"
     url "https://github.com/danBamikiya/homebrew-hello/releases/download/v1.0.0/hello-macos-x64.tar.gz"
-    sha256
-    "c2da856bd7b824febec4d7151ae9cadf90d049c90dc56a040d5e297c2a272d01"
+    sha256 "c2da856bd7b824febec4d7151ae9cadf90d049c90dc56a040d5e297c2a272d01"
     version "1.0.0"
 
     def install
